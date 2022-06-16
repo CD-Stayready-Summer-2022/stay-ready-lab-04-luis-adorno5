2c6de9199ec3367e06f987b800627ef544d8a04f
Luis Adorno <luis.adorno5@upr.edu>
2022-06-16 13:19:48 -0400

    Selection and Bubble Sort implementatioms with related tests.

--- src/main/java/com/codedifferently/SortExample.java
+++ src/main/java/com/codedifferently/SortExample.java
@@ -1,4 +1,48 @@
 package com.codedifferently;
 
 public class SortExample {
+
+    public static void main(String[] args) {
+        int[] numbers = {40, 7, 59, 4, 1};
+        displayValues(numbers);
+        int[] sortedNumbers = bubbleSort(numbers);
+        displayValues(sortedNumbers);
+    }
+
+    public static int[] selectionSort(int[] numbers){
+        int indexMin = 0;
+        for(int i = 0; i < numbers.length; i++){
+            indexMin = i;
+            for(int j = i + 1; j < numbers.length; j++){
+                if(numbers[j] < numbers[indexMin]){
+                    indexMin = j;
+                }
+            }
+            int temp = numbers[i];
+            numbers[i] = numbers[indexMin];
+            numbers[indexMin] = temp;
+        }
+        return numbers;
+    }
+
+    public static int[] bubbleSort(int[] numbers){
+        for (int i = 0; i< numbers.length; i++){
+            for(int j = 0; j< numbers.length-1; j++){
+                if(numbers[j] > numbers[j+1]){
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1] = temp;
+                }
+            }
+        }
+        return numbers;
+    }
+
+    public static void displayValues(int[] numbers){
+        for(int num : numbers){
+            System.out.print(num + " ");
+        }
+        System.out.println("\n");
+    }
+
 }
--- src/test/java/com/codedifferently/SortExampleTest.java
+++ src/test/java/com/codedifferently/SortExampleTest.java
@@ -1,4 +1,33 @@
 package com.codedifferently;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class SortExampleTest {
+
+    private int[] numbers;
+
+    @BeforeEach
+    public void init(){
+        numbers = new int[]{40, 7, 59, 4, 1};
+    }
+
+    @Test
+    @DisplayName("Selection sort test.")
+    public void selectionSortTest01(){
+        int[] expected = {1, 4, 7, 40, 59};
+        int[] actual = SortExample.selectionSort(numbers);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Bubble sort test.")
+    public void bubbleSortTest01(){
+        int[] expected = {1, 4, 7, 40, 59};
+        int[] actual = SortExample.bubbleSort(numbers);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
 }
